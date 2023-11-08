cf85f4e986decb3dbdc5bb3b4b5e13601d883d2e
Adrian Hossen <adrianhossen5@gmail.com>
2023-11-08 01:11:22 -0500

    WIP Profile

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,9 +1,45 @@
-import { signOut, useSession } from 'next-auth/react';
-import React from 'react';
+import { getSession, signOut, useSession } from 'next-auth/react';
+import React, { useState } from 'react';
 import router from 'next/router';
+import prisma from '@/lib/prismadb';
+import { Profile } from '@prisma/client';
 
-const Dashboard = () => {
+export async function getServerSideProps(context: any) {
+  try {
+    const session = await getSession(context);
+    const currUser = session?.user;
+
+    const user = await prisma.user.findFirst({
+      where: { email: currUser?.email! }
+    })
+
+    const profile = await prisma.profile.findFirst({
+      where: { userId: user?.id! },
+    });
+
+    if (profile === null || profile === undefined)
+    {
+      router.push('/Profile');
+    }
+
+    return {
+      props: {
+        profile: profile,
+      },
+    };
+  } catch (error) {
+    const profile = null;
+    return {
+      props: {
+        profile: profile,
+      },
+    };
+  }
+}
+
+const Dashboard = ({profile}: {profile: any}) => {
   const { status: sesh, data: data } = useSession();
+  const [userProfile] = useState<Profile>(profile);
 
   if (sesh === 'loading') {
     return null;
@@ -13,10 +49,14 @@ const Dashboard = () => {
     router.push('/');
   }
 
+  if (userProfile === null) {
+    router.push('/Profile');
+  }
+
   if (sesh === 'authenticated') {
     return (
       <div className="py-10">
-        <div>Hi {data.user?.email}</div>
+        <div>Hi {userProfile.id}</div>
         <button
           onClick={() =>
             signOut({ callbackUrl: '/' })
