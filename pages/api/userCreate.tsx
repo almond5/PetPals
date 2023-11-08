08b4b551d4720e83a3747bd1ff81d2430ba86efa
Adrian Hossen <adrianhossen5@gmail.com>
2023-11-08 00:24:32 -0500

    Migration

--- pages/api/userCreate.tsx
+++ pages/api/userCreate.tsx
@@ -34,6 +34,7 @@ export default async function handler(
           email: userEmail,
           password: password,
           phoneNumber: phoneNumber,
+          name: ""
         },
       });
       res.status(200).json('Success');
