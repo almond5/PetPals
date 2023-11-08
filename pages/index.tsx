dc9de257aa6c944528befc15e82119d37e793cba
Heng Hong <118714008+pv314@users.noreply.github.com>
2023-11-08 11:45:36 -0500

    Add files via upload

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,46 @@
+// import { signIn } from 'next-auth/react';
+// import Link from 'next/link';
+
+// const Index = () => {
+//   return (
+//     <div>
+//       <div>
+//         <button>
+//           {' '}
+//           <Link
+//             href={'/api/auth/signin'}
+//             onClick={(e) => {
+//               e.preventDefault();
+//               signIn('Credentials', { callbackUrl: '/Dashboard' });
+//             }}
+//           >
+//             Click me to Sign In
+//           </Link>
+//         </button>
+//       </div>
+//       <div>
+//         <button>
+//           {' '}
+//           <Link href="/Register">Click Me to Register </Link>
+//         </button>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Index;
+
+
 import { signIn } from 'next-auth/react';
 import Link from 'next/link';
+import styles from '../styles/Index.module.css'; // Make sure to create this CSS module
 
 const Index = () => {
   return (
-    <div>
+    <div className={styles.container}>
+      <img src="/img/index.png" alt="Pet and Mouse" className={styles.petImage} />
       <div>
-        <button>
-          {' '}
+        <button className={`${styles.button} ${styles.signInButton}`}>
           <Link
             href={'/api/auth/signin'}
             onClick={(e) => {
@@ -14,14 +48,11 @@ const Index = () => {
               signIn('Credentials', { callbackUrl: '/Dashboard' });
             }}
           >
-            Click me to Sign In
+            login
           </Link>
         </button>
-      </div>
-      <div>
-        <button>
-          {' '}
-          <Link href="/Register">Click Me to Register </Link>
+        <button className={`${styles.button} ${styles.registerButton}`}>
+          <Link href="/Register">SIGN UP</Link>
         </button>
       </div>
     </div>
