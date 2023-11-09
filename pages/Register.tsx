0a6e4cb3372f83531658a9d79e855e6509fb95da
kaih969 <131720400+kaih969@users.noreply.github.com>
2023-11-09 13:04:01 -0500

    Add files via upload

--- pages/Register.tsx
+++ pages/Register.tsx
@@ -2,6 +2,7 @@ import { signIn } from 'next-auth/react';
 import Link from 'next/link';
 import router from 'next/router';
 import { useState } from 'react';
+import styles from '/styles/Index.module.css';
 
 const Register = () => {
   const [password, setPassword] = useState('');
@@ -51,56 +52,159 @@ const Register = () => {
     setPassword('');
     setUserEmail('');
   };
+  
+
+                            // Kai's version
 
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-200">
-      <div className="w-full max-w-xs">
-        <form
-          className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 text-center"
-          onSubmit={handleSubmit}
+    <div className={styles.container}>
+      <img
+        src="/img/FormContainer.png"
+        alt="Background"
+        className={styles.petImage}
+      />
+      <form
+        className="form bg-write rounded px-8 pt-6 pb-8 mb-4 text-center"
+        onSubmit={handleSubmit}
+        style={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)' }} // Centers the form on the image
+
         >
-          <div className="mb-4">
+        <div className="mt-24 flex flex-auto">
+          <img
+            src="/img/username.png"
+            alt="Pet and Mouse"
+            className={styles.username}
+          />
             <input
               onChange={(e) => setUserEmail(e.target.value)}
               type="email"
               id="email"
-              placeholder="Enter your Email"
-              className="input-field w-full"
+              placeholder="Enter your email"
+              className="border-none outline-none absolute mt-16 ml-6 w-[350px]"
             />
-          </div>
-          <div className="mb-4">
-            <input
-              onChange={(e) => handlePassword(e)}
-              type="password"
-              id="password"
-              placeholder="Enter your Password"
-              required
-              className="input-field w-full"
-            />
-          </div>
-          <div className="mb-4">
-            <input
+        </div>
+
+        <div className="mt-4 mb-6 flex flex-auto">
+          <img
+            src="/img/password.png"
+            alt="Pet and Mouse"
+            className={styles.username}
+          />
+          <input
+            onChange={(e) => handlePassword(e)}
+            type="password"
+            id="password"
+            required
+            placeholder="Enter your password"
+            className="border-none outline-none absolute mt-16 ml-6 w-[350px]"
+          />
+        </div>
+
+        <div className="mt-4 mb-6 flex flex-auto">
+          <img
+            src="/img/phone.png"
+            alt="Pet and Mouse"
+            className={styles.username}
+          />
+          <input
               onChange={(e) => setPhoneNumber(e.target.value)}
               type="text"
               pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
               id="phone_number"
-              placeholder="Phone# Ex: (888-888-8888)"
-              className="input-field w-full"
-            />
-          </div>
-          <button type="submit">Submit</button>
-          <div className="text-center mt-6">
-          <Link
+              placeholder="888-888-8888"
+            className="border-none outline-none absolute mt-16 ml-6 w-[350px]"
+          />
+        </div>
+
+        <div className="flex items-center justify-center mt-4">
+          <button
+            type="submit"
+            className="signUpButton"
+          >
+            <img src="/img/registerSignUp.png" alt="Sign Up" />
+          </button>
+        </div>
+
+        <Link
             href={'/api/auth/signin'}
             onClick={(e) => {
               e.preventDefault();
               signIn('Credentials', { callbackUrl: '/' });
             }}
+            style={{ textDecoration: 'underline' }}
           >Already have an account?</Link>
-          </div>
-        </form>
+
+      </form>
+
       </div>
-    </div>
+
+
+
+            // original version, just in case need to recover
+
+
+
+
+      // <div className="min-h-screen flex items-center justify-center bg-gray-200">
+      // <div className="w-full max-w-xs">
+      //   <form
+      //     className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 text-center"
+      //     onSubmit={handleSubmit}
+      //   >
+      //     <div className="mb-4">
+      //       <input
+      //         onChange={(e) => setUserEmail(e.target.value)}
+      //         type="email"
+      //         id="email"
+      //         placeholder="Enter your Email"
+      //         className="input-field w-full"
+      //       />
+      //     </div>
+          // <div className="mb-4">
+          //   <input
+          //     onChange={(e) => handlePassword(e)}
+          //     type="password"
+          //     id="password"
+          //     placeholder="Enter your Password"
+          //     required
+          //     className="input-field w-full"
+          //   />
+          // </div>
+
+          // <div className="mb-4">
+          //   <input
+          //     onChange={(e) => handlePassword(e)}
+          //     type="password"
+          //     id="password"
+          //     placeholder="Enter your Password"
+          //     required
+          //     className="input-field w-full"
+          //   />
+          // </div>
+
+          // <div className="mb-4">
+          //   <input
+              // onChange={(e) => setPhoneNumber(e.target.value)}
+              // type="text"
+              // pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
+              // id="phone_number"
+              // placeholder="Phone# Ex: (888-888-8888)"
+              // className="input-field w-full"
+          //   />
+          // </div>
+      //     <button type="submit">Submit</button>
+      //     <div className="text-center mt-6">
+          // <Link
+          //   href={'/api/auth/signin'}
+          //   onClick={(e) => {
+          //     e.preventDefault();
+          //     signIn('Credentials', { callbackUrl: '/' });
+          //   }}
+          // >Already have an account?</Link>
+          // </div>
+      //   </form>
+      // </div>
+    // </div>
   );
 };
 
