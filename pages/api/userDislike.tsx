69c54c635c087b2e2a0d094b31592330515cdf6c
Hemanka <hkanoujia02@gmail.com>
2023-11-17 12:37:21 -0500

    like and dislike api - need to be tested

---
+++ pages/api/userDislike.tsx
@@ -0,0 +1,89 @@
+// STATUS: NEED TO BE TESTED
+
+import prisma from '@/lib/prismadb';
+import { Prisma } from '@prisma/client';
+import { NextApiRequest, NextApiResponse } from 'next';
+
+export default async function like(
+    req: NextApiRequest,
+    res: NextApiResponse
+) {
+    if (req.method === 'POST') 
+    {
+        try
+        {
+            let currProfileId, currInterestedProfileId: any;
+
+            if (typeof req.body === 'object')
+            {
+                currProfileId: req.body.profileId;
+                currInterestedProfileId: req.body.interestedProfileId;
+            }
+            else
+            {
+                const body = JSON.parse(req.body);
+                currProfileId: body.profileId;
+                currInterestedProfileId: body.interestedProfileId;
+            }
+
+            // change the 'isMatch' to 'False' for the given id relation
+            const info  = await prisma.interest.upsert({
+                where: {
+                    profileId: currProfileId,
+                    interestedProfileId: currInterestedProfileId,
+                },
+                data: {
+                    isMatch: "False",
+                },
+                create: {
+                    profileId: currProfileId,
+                    interestedProfileId: currInterestedProfileId,
+                    isMatch: "False",
+                },
+            });
+            res.status(200).json("Success");
+
+            // const infoExist = await prisma.interest.findFirst({
+            //     where: {
+            //         profileId: currProfileId,
+            //         interestedProfileId: currInterestedProfileId,
+            //     },
+            // });
+
+            // // change the 'isMatch' to 'False' for the given id relation
+            // if (infoExist !== null)
+            // {
+            //     const addInfo = await prisma.interest.create({
+            //         data: {
+            //             profileId: currProfileId,
+            //             interestedProfileId: currInterestedProfileId,
+            //             isMatch: "False",
+            //         }
+            //     });
+            //     res.status(200).json("Created Successfully");
+            // }
+            // else
+            // {
+            //     const updateInfo = await prisma.interest.update({
+            //         where: {
+            //             profileId: currProfileId,
+            //             interestedProfileId: currInterestedProfileId,
+            //         },
+            //         data: {
+            //             isMatch: "False",
+            //         }
+            //     });
+            //     res.status(200).json("Updated Successfully");
+            // }
+        }
+        catch (error)
+        {
+            console.log(error);
+            res.status(500).json('Unknown Error Occurred');
+        }
+    }
+    else
+    {
+        res.status(405).json(null);
+    }
+}
---
+++ pages/api/userLike.tsx
@@ -0,0 +1,93 @@
+// STATUS: NEED TO BE TESTED
+
+import prisma from '@/lib/prismadb';
+import { Prisma } from '@prisma/client';
+import { NextApiRequest, NextApiResponse } from 'next';
+
+export default async function like(
+    req: NextApiRequest,
+    res: NextApiResponse
+) {
+    if (req.method === 'POST') 
+    {
+        try
+        {
+            let currProfileId, currInterestedProfileId: any;
+
+            if (typeof req.body === 'object')
+            {
+                currProfileId: req.body.profileId;
+                currInterestedProfileId: req.body.interestedProfileId;
+            }
+            else
+            {
+                const body = JSON.parse(req.body);
+                currProfileId: body.profileId;
+                currInterestedProfileId: body.interestedProfileId;
+            }
+
+            // if the interest between the two profile is not in the db
+            // store it with the match status being pending; otherwise
+            // update 'isMatch' to 'True'
+            const info  = await prisma.interest.upsert({
+                where: {
+                        profileId: currProfileId,
+                        interestedProfileId: currInterestedProfileId,
+                },
+                data: {
+                    isMatch: "True",
+                },
+                create: {
+                    profileId: currProfileId,
+                    interestedProfileId: currInterestedProfileId,
+                    isMatch: "Pending",
+                },
+            });
+            res.status(200).json("Success");
+
+            // const infoExist = await prisma.interest.findFirst({
+            //     where: {
+            //         profileId: currProfileId,
+            //         interestedProfileId: currInterestedProfileId,
+            //     },
+            // });
+
+            // // if the interest between the two profile is not in the db
+            // // store it with the match status being pending; otherwise
+            // // update 'isMatch' to 'True'
+            // if (infoExist !== null)
+            // {
+            //     const addInfo = await prisma.interest.create({
+            //         data: {
+            //             profileId: currProfileId,
+            //             interestedProfileId: currInterestedProfileId,
+            //             isMatch: "Pending",
+            //         }
+            //     });
+            //     res.status(200).json("Created Successfully");
+            // }
+            // else
+            // {
+            //     const updateInfo = await prisma.interest.update({
+            //         where: {
+            //             profileId: currProfileId,
+            //             interestedProfileId: currInterestedProfileId,
+            //         },
+            //         data: {
+            //             isMatch: "True",
+            //         },
+            //     });
+            //     res.status(200).json("Updated Successfully");
+            // }
+        }
+        catch (error)
+        {
+            console.log(error);
+            res.status(500).json('Unknown Error Occurred');
+        }
+    }
+    else
+    {
+        res.status(405).json(null);
+    }
+}
