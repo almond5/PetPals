8a0f690394339b7c28ffba8ee6d2f18260e975e7
Adrian Hossen <adrianhossen5@gmail.com>
2023-11-17 14:48:55 -0500

    WIP

--- pages/api/userDislike.tsx
+++ pages/api/userDislike.tsx
@@ -1,72 +1,57 @@
-// STATUS: NEED TO BE TESTED
-
 import prisma from '@/lib/prismadb';
-import { Prisma } from '@prisma/client';
 import { NextApiRequest, NextApiResponse } from 'next';
 
-export default async function like(
-    req: NextApiRequest,
-    res: NextApiResponse
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
 ) {
-    if (req.method === 'POST') 
-    {
-        try
-        {
-            let currProfileId, currInterestedProfileId: any;
+  if (req.method === 'POST') {
+    try {
+      let currProfileId, currInterestedProfileId: any;
 
-            if (typeof req.body === 'object')
-            {
-                currProfileId: req.body.profileId;
-                currInterestedProfileId: req.body.interestedProfileId;
-            }
-            else
-            {
-                const body = JSON.parse(req.body);
-                currProfileId: body.profileId;
-                currInterestedProfileId: body.interestedProfileId;
-            }
+      if (typeof req.body === 'object') {
+        currProfileId = req.body.profileId;
+        currInterestedProfileId = req.body.interestedProfileId;
+      } else {
+        const body = JSON.parse(req.body);
+        currProfileId = body.profileId;
+        currInterestedProfileId = body.interestedProfileId;
+      }
 
-            const infoExist = await prisma.interest.findFirst({
-                where: {
-                    profileId: currProfileId,
-                    interestedProfileId: currInterestedProfileId,
-                },
-            });
+      const infoExist = await prisma.interest.findFirst({
+        where: {
+          profileId: currProfileId,
+          interestedProfileId: currInterestedProfileId,
+        },
+      });
 
-            // change the 'isMatch' to 'False' for the given id relation
-            if (infoExist !== null)
-            {
-                const addInfo = await prisma.interest.create({
-                    data: {
-                        profileId: currProfileId,
-                        interestedProfileId: currInterestedProfileId,
-                        isMatch: "False",
-                    }
-                });
-                res.status(200).json("Created Successfully");
-            }
-            else
-            {
-                const updateInfo = await prisma.interest.updateMany({
-                    where: {
-                        profileId: currProfileId,
-                        interestedProfileId: currInterestedProfileId,
-                    },
-                    data: {
-                        isMatch: "False",
-                    }
-                });
-                res.status(200).json("Updated Successfully");
-            }
-        }
-        catch (error)
-        {
-            console.log(error);
-            res.status(500).json('Unknown Error Occurred');
-        }
+      // change the 'isMatch' to 'False' for the given id relation
+      if (infoExist !== null) {
+        const addInfo = await prisma.interest.create({
+          data: {
+            profileId: currProfileId,
+            interestedProfileId: currInterestedProfileId,
+            isMatch: 'False',
+          },
+        });
+        res.status(200).json('Created Successfully');
+      } else {
+        const updateInfo = await prisma.interest.updateMany({
+          where: {
+            profileId: currProfileId,
+            interestedProfileId: currInterestedProfileId,
+          },
+          data: {
+            isMatch: 'False',
+          },
+        });
+        res.status(200).json('Updated Successfully');
+      }
+    } catch (error) {
+      console.log(error);
+      res.status(500).json('Unknown Error Occurred');
     }
-    else
-    {
-        res.status(405).json(null);
-    }
+  } else {
+    res.status(405).json(null);
+  }
 }
