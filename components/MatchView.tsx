f2251fc32fb87fc7d8c55aff1051a9ddf139c25c
Adrian Hossen <adrianhossen5@gmail.com>
2023-11-17 13:55:24 -0500

    WIP

---
+++ components/MatchView.tsx
@@ -0,0 +1,21 @@
+// import React, { useState } from 'react';
+// import { PetProfile } from '@prisma/client';
+// import styles from '/styles/dashboard.module.css';
+
+// const MatchView = (props: { petProfiles: any; petProfile: any }) => {
+//   const [profile] = useState<PetProfile>(props.petProfile);
+//   const [profiles] = useState<PetProfile[]>(props.petProfiles);
+//   const [showDescription, setShowDescription] = useState(false);
+
+//   const toggleDescription = () => {
+//     setShowDescription(!showDescription);
+//   };
+
+//   return (
+//     <div className="py-20 flex justify-center">
+      
+//     </div>
+//   );
+// };
+
+// export default MatchView;
