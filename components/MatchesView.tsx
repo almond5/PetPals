426a4f740a5a108b75c0130e5b5115e0b3b4c103
David Fanai <liandavid39@gmail.com>
2023-11-18 19:52:04 -0500

    WIP Scroll

--- components/MatchesView.tsx
+++ components/MatchesView.tsx
@@ -8,18 +8,20 @@ const MatchesView = (props: { matches: any; petProfile: any }) => {
   return (
     <div className='py-10'>
       <div className="flex justify-center [font-family:'Mali-Regular',Helvetica] font-normal text-black text-[24px] text-center tracking-[0] leading-[normal]">Matches</div>
-      {matches.map((petProfile: any) => (
-        <div key={petProfile.id} className="my-2">
-          <div className = "flex justify-center items-center">
-            <div className="w-[300px] h-[90px] border border-solid border-black relative">
-              <div className="left-[18px] absolute w-[50px] h-[50px] top-[19px] bg-[#d9d9d9] rounded-[25px]" />
-              <div className="left-[88px] absolute top-[28px] [font-family:'Mali-Regular',Helvetica] font-normal text-black text-[24px] text-center tracking-[0] leading-[normal]">
-                {petProfile.name}
+      <div className="matches-container" style={{ maxHeight: '400px', overflowY: 'auto' }}>
+        {matches.map((petProfile: any) => (
+          <div key={petProfile.id} className="my-2">
+            <div className="flex justify-center items-center">
+              <div className="w-[300px] h-[90px] border border-solid border-black relative">
+                <div className="left-[18px] absolute w-[50px] h-[50px] top-[19px] bg-[#d9d9d9] rounded-[25px]" />
+                <div className="left-[88px] absolute top-[28px] [font-family:'Mali-Regular',Helvetica] font-normal text-black text-[24px] text-center tracking-[0] leading-[normal]">
+                  {petProfile.name}
+                </div>
               </div>
             </div>
           </div>
-        </div>
-      ))}
+        ))}
+      </div>
     </div>
   );
 };
