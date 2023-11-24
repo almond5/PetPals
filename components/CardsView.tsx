d549aaf8a464ddc6bccb46c9bbfba679bc41e5bf
Adrian Hossen <adrianhossen5@gmail.com>
2023-11-24 11:54:28 -0500

    Revert

--- components/CardsView.tsx
+++ components/CardsView.tsx
@@ -218,7 +218,7 @@ const CardsView = (props: {
                           removeItem(petProfile.id);
                         }}
                       >
-                        <img src="/img/vector.png"></img>
+                        <img src="/img/Vector.png"></img>
                       </button>{' '}
                     </div>
                   </div>
